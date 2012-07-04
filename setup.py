72409d275bea47196dd3b739b96390d913a38802
Tom Aldcroft <taldcroft@gmail.com>
2012-07-04 15:20:57 -0400

    Add npytypes package setup.py and remove subpackage setup.py files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup, Extension
+import numpy as np
+
+ext_modules = []
+
+ext = Extension('npytypes.rational',
+                sources=['npytypes/rational/rational.c'],
+                include_dirs=[np.get_include()])
+ext_modules.append(ext)
+
+ext = Extension('npytypes.quaternion.numpy_quaternion',
+                sources=['npytypes/quaternion/quaternion.c',
+                         'npytypes/quaternion/numpy_quaternion.c'],
+                include_dirs=[np.get_include()],
+                extra_compile_args=['-std=c99'])
+ext_modules.append(ext)
+
+setup(name='npytypes',
+      version='0.1',
+      description='NumPy type extensions',
+      packages=['npytypes',
+                'npytypes.quaternion',
+                # 'npytypes.rational'
+                ],
+      ext_modules=ext_modules)

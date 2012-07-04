6170017e621b3245c30f3c377ce5e9c480ff0aa1
Tom Aldcroft <taldcroft@gmail.com>
2012-07-04 14:33:57 -0400

    Rework quaternion setup.py to use distutils Extension not numpy Configuration

--- npytypes/quaternion/setup.py
+++ npytypes/quaternion/setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
-def configuration(parent_package='',top_path=None):
-    import numpy
-    from distutils.errors import DistutilsError
-    if numpy.__dict__.get('quaternion') is not None:
-        raise DistutilsError('The target NumPy already has a quaternion type')
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('quaternion',parent_package,top_path)
-    config.add_extension('numpy_quaternion',['quaternion.h','quaternion.c','numpy_quaternion.c'])
-    return config
 
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(configuration=configuration)
+from distutils.core import setup, Extension
+
+import numpy as np
+from distutils.errors import DistutilsError
+
+if np.__dict__.get('quaternion') is not None:
+    raise DistutilsError('The target NumPy already has a quaternion type')
+
+quat_ext = Extension('numpy_quaternion',
+                     sources=['quaternion.c',
+                              'numpy_quaternion.c'],
+                     include_dirs=[np.get_include()],
+                     extra_compile_args=['-std=c99'])
+
+setup(name='quaternion',
+      version='1.0',
+      description='Quaternion NumPy dtype',
+      ext_modules=[quat_ext])

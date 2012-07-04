71d8083b8525f5ea50d25c610d1f98e25ac77b7f
Tom Aldcroft <taldcroft@gmail.com>
2012-07-04 14:30:45 -0400

    Use numpy.get_include() instead of distutils.system_info.get_info()

--- npytypes/rational/setup.py
+++ npytypes/rational/setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-from numpy.distutils.system_info import get_info
+# from numpy.distutils.system_info import get_info
+import numpy as np
 
 module = Extension('rational',
                    sources = ['rational.c'],
                    # extra_compile_args = ['-g'],
-                   include_dirs = get_info('numpy')['include_dirs'])
+                   include_dirs = [np.get_include()])
 
 setup(name = 'rational',
       version = '1.0',
